1dfb0c8a21b1fbf53168ab48345553222d36045b
nilslambertz <nilslambertz@web.de>
2021-06-17 01:39:36 +0200

    added method to add account, currently triggered by clicking one of the upper lists

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.scss';
 import UpperList from "./Components/UpperList/UpperList";
-import {account, accountArray} from "./Utils/Interfaces";
+import {account} from "./Utils/Interfaces";
 import Account from "./Components/Account/Account";
+import {generateKeyAddressPair} from "./Utils/Functions";
+
+const crypto = require('crypto');
+const primeLength = 120;
 
 interface AppProps {
 }
 
 interface AppState {
     idCount: number,
-    accounts: accountArray,
+    accounts: account[],
 }
 
 class App extends React.Component<AppProps, AppState> {
@@ -22,12 +26,33 @@ class App extends React.Component<AppProps, AppState> {
         };
     }
 
+    addAccount = (random?: boolean) : void => {
+        let keys = generateKeyAddressPair();
+        let count = this.state.idCount;
+
+        let a : account = {
+            id: count,
+            privateKey: keys.privateKey,
+            address: keys.address
+        }
+        this.setState({idCount: count + 1})
+
+        if(random) {
+            a.balance = Math.floor(Math.random() * 1001);
+        }
+
+        let arr : account[] = this.state.accounts;
+        arr.push(a);
+        this.setState({accounts: arr});
+        console.log(a);
+    }
 
     render() {
         return <div className="App">
-            <div id={"upperContent"}>
+            <div id={"upperContent"} onClick={() => this.addAccount(true)}>
                 <UpperList
                     title={"accounts"}
+                    accounts={this.state.accounts}
                 />
                 <UpperList
                     title={"transactions"}
