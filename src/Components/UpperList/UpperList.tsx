d01014626b85def5f477bac95ef8134e0ac1225c
nilslambertz <nilslambertz@web.de>
2021-06-17 01:37:26 +0200

    implemented first print-method for accounts

--- src/Components/UpperList/UpperList.tsx
+++ src/Components/UpperList/UpperList.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import './UpperList.scss';
-import {account, transcation} from "../../Utils/Interfaces";
+import {account, settings, transcation} from "../../Utils/Interfaces";
+import Account from "../Account/Account";
 
 interface UpperListProps {
     title: string;
-    list?: account[] | transcation[]
+    accounts?: account[],
+    transactions?: transcation[],
+    settings?: settings[]
 }
 
 class UpperList extends React.Component<UpperListProps, {}> {
     render() {
+        console.log("XDDD");
+
+        let printFunction : any = (err : any) => {return <div className={"listError"}>{err}</div>};
+        let arg : any = "Error";
+        if(this.props.accounts) {
+            printFunction = this.printAccountList;
+            arg = this.props.accounts;
+        }
+
         return <div className={"upperList"}>
             <div className={"upperListTitle"}>{this.props.title}</div>
-            <div>xd</div>
+            <div>{printFunction(arg)}</div>
         </div>;
+    }
+
+    printAccountList(accounts : account[]) {
+        console.log("DDXCSADC");
+        return accounts.map(function (value, index, array) {
+            return <Account account={value} />;
+        });
     }
 }
 
