c2524b461efbff0a45b3ac4a0cdc93687c67e0de
nilslambertz <nilslambertz@web.de>
2021-06-17 01:39:07 +0200

    added simple table to display account values

--- src/Components/Account/Account.tsx
+++ src/Components/Account/Account.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {account} from "../../Utils/Interfaces";
+import "./Account.scss"
 
 interface AccountProps {
     account: account
@@ -8,7 +9,20 @@ interface AccountProps {
 class Account extends React.Component<AccountProps, {}> {
     render() {
         return <div className={"account"}>
-            <div>{this.props.account.privateKey}</div>
+            <table className={"accountTable"}>
+                <tr>
+                    <td className={"id"}>{this.props.account.id}</td>
+                    <td className={"privateKey"}>{this.props.account.privateKey}</td>
+                    <td className={"address"}>{this.props.account.address}</td>
+                    <td className={"balance"}>{this.props.account.balance}</td>
+                </tr>
+                <tr>
+                    <td>ID</td>
+                    <td>Private Key</td>
+                    <td>Address</td>
+                    <td>Balance</td>
+                </tr>
+            </table>
         </div>;
     }
 }
