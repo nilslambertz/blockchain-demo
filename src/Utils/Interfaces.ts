2d759ff48270129311cad203a20407b76d2682a5
nilslambertz <nilslambertz@web.de>
2021-06-17 01:38:39 +0200

    removed array-interfaces, added keyAddressPair-interface

--- src/Utils/Interfaces.ts
+++ src/Utils/Interfaces.ts
@@ -1,16 +1,8 @@
-export interface accountArray {
-    [index: number]: account
-}
-
 export interface account {
     id: number,
     privateKey?: string,
-    publicKey?: string,
+    address?: string,
     balance?: number
-}
-
-export interface transactionArray {
-    [index: number]: transcation
 }
 
 export interface transcation {
@@ -21,13 +13,14 @@ export interface transcation {
     signature?: string
 }
 
-export interface settingsArray {
-    [index: number]: settings
-}
-
 export interface settings {
     name: string,
     toggle: boolean,
     possibleValues?: string[],
     currentState: string | boolean
+}
+
+export interface keyAddressPair {
+    privateKey: string,
+    address: string
 }
