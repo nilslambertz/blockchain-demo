f081911062c99fa9efe83a7857fa1d933bb2d09c
nilslambertz <nilslambertz@web.de>
2021-06-18 01:01:56 +0200

    added tbody inside table

--- src/Components/Account/Account.tsx
+++ src/Components/Account/Account.tsx
@@ -10,18 +10,20 @@ class Account extends React.Component<AccountProps, {}> {
     render() {
         return <div className={"account"}>
             <table className={"accountTable"}>
-                <tr>
-                    <td className={"id"}>{this.props.account.id}</td>
-                    <td className={"privateKey smallText"}>{this.props.account.privateKey}</td>
-                    <td className={"address smallText"}>{this.props.account.address}</td>
-                    <td className={"balance"}>{this.props.account.balance}</td>
-                </tr>
-                <tr className={"description"}>
-                    <td>ID</td>
-                    <td>Private Key</td>
-                    <td>Address</td>
-                    <td>Balance</td>
-                </tr>
+                <tbody>
+                    <tr>
+                        <td className={"id"}>{this.props.account.id}</td>
+                        <td className={"privateKey smallText"}>{this.props.account.privateKey}</td>
+                        <td className={"address smallText"}>{this.props.account.address}</td>
+                        <td className={"balance"}>{this.props.account.balance}</td>
+                    </tr>
+                    <tr className={"description"}>
+                        <td>ID</td>
+                        <td>Private Key</td>
+                        <td>Address</td>
+                        <td>Balance</td>
+                    </tr>
+                </tbody>
             </table>
         </div>;
     }
