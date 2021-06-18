b953889871628960a4ac611637d68bcb4b679f9a
nilslambertz <nilslambertz@web.de>
2021-06-19 01:27:43 +0200

    added style and basic display of blocks

--- src/Components/Blockchain/Block.tsx
+++ src/Components/Blockchain/Block.tsx
@@ -3,13 +3,20 @@ import "./Blockchain.scss";
 import {block} from "../../Utils/Interfaces";
 
 interface BlockProps {
-    block?: block
+    block: block
 }
 
 class Block extends React.Component<BlockProps, {}> {
     render() {
+        let confirmed : string = this.props.block.confirmed ? "confirmed" : "unconfirmed";
+
         return <div className={"blockContainer"}>
-            alge
+            <div className={"block " + confirmed}>
+                <div className={"prevHash"}>{this.props.block?.prevHash}</div>
+                <div className={"transactions"}>test</div>
+                <div className={"nonce"}>{this.props.block?.nonce}</div>
+                <div className={"confirmed"}>{confirmed}</div>
+            </div>
         </div>;
     }
 }
--- src/Components/Blockchain/Blockchain.tsx
+++ src/Components/Blockchain/Blockchain.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 import "./Blockchain.scss";
 import {block} from "../../Utils/Interfaces";
 import Block from "./Block";
+import {ReactComponent as Arrow} from "../../res/arrowright.svg";
 
 interface BlockchainProps {
-    blocks?: block[]
+    blocks: block[]
 }
 
 class Blockchain extends React.Component<BlockchainProps, {}> {
     render() {
         return <div className={"blockchain"}>
-            <Block /><div className={"arrows"} /> <Block/>
+            {this.printBlocks()}
         </div>;
+    }
+
+    printBlocks = () => {
+        return this.props.blocks.map(function (value, index, array) {
+            return (
+                <React.Fragment>
+                    <Block block={value} />
+                    {index !== array.length -1 ? <div className={"arrows"} ><Arrow /></div> : "" }
+                </React.Fragment>
+            );
+        });
     }
 }
 
