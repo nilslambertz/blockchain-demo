1d428ecdefcffd99b29032c5d630c221884811c4
nilslambertz <nilslambertz@web.de>
2021-06-20 01:14:29 +0200

    added drag and drop to UpperList, added needed props

--- src/Components/UpperList/UpperList.tsx
+++ src/Components/UpperList/UpperList.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {createRef, useRef} from 'react';
 import './UpperList.scss';
 import {account, settings, transcation} from "../../Utils/Interfaces";
 import Account from "../Account/Account";
 import Transaction from "../Transaction/Transaction";
+import {DragDropContext, Droppable} from "react-beautiful-dnd";
 
 interface UpperListProps {
     title: string;
@@ -13,10 +14,16 @@ interface UpperListProps {
     className?: string
     addFunction?: any
     signFunction?: any,
-    removeSignatureFunction?: any
+    removeSignatureFunction?: any,
+    placeholder?: any,
+    innerRef?: any
 }
 
 class UpperList extends React.Component<UpperListProps, {}> {
+    setRef = (ref : any) => {
+        if(this.props.innerRef) this.props.innerRef(ref);
+    }
+
     render() {
         let printFunction : any = (err : any) => {return <div className={"listError"}>{err}</div>};
         let arg : any = "Error";
@@ -31,23 +38,32 @@ class UpperList extends React.Component<UpperListProps, {}> {
         let addFunction = this.props.addFunction;
         if(!addFunction) addFunction = () => {console.log("Error: function is not defined")};
 
-        return <div className={"upperListContainer " + this.props.className}>
+        let p = "";
+        if(this.props.placeholder) p = this.props.placeholder;
+
+        return (<div className={"upperListContainer " + this.props.className} ref={this.setRef}>
             <div className={"upperListTitle"}>{this.props.title}</div>
-            <div className={"upperList"}>
-                {printFunction(arg)}
-            </div>
+                        <div className={"upperList"}>
+                            {printFunction(arg)}
+                            {p}
+                        </div>
+
             <div className={"addButtonContainer"}>
                 <div className={"addButton"} onClick={() => addFunction()}>
                     Add
                 </div>
             </div>
-        </div>;
+        </div>);
     }
 
     printAccountList(accounts : account[]) {
         return accounts.map(function (value, index, array) {
             return <Account account={value} key={value.id} />;
         });
+    }
+
+    onDragEnd = () => {
+        // TODO
     }
 
     printTransactionList = (transactions : transcation[]) => {
@@ -58,9 +74,10 @@ class UpperList extends React.Component<UpperListProps, {}> {
         let signFunction = this.props.signFunction;
         let removeSignatureFunction = this.props.removeSignatureFunction;
 
-        return transactions.map(function (value, index, array) {
-            return <Transaction transaction={value} numberOfAccounts={numberOfAccounts} key={value.id} signFunction={signFunction} removeSignatureFunction={removeSignatureFunction}/>;
-        });
+        return (transactions.map(function (value, index, array) {
+            return <Transaction transaction={value} numberOfAccounts={numberOfAccounts} key={value.id}
+                                signFunction={signFunction} removeSignatureFunction={removeSignatureFunction} index={index}/>
+        }));
     }
 }
 
