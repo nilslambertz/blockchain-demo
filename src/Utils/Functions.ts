cf530e51a83d03398f980c728d119cb46ac93ff0
nilslambertz <nilslambertz@web.de>
2021-06-22 01:53:13 +0200

    implemented function to calculate hash of block

--- src/Utils/Functions.ts
+++ src/Utils/Functions.ts
@@ -1,7 +1,9 @@
-import {keyAddressPair, signaturePair, transcation} from "./Interfaces";
+import {account, block, keyAddressPair, signaturePair, transcation} from "./Interfaces";
 import nacl, {BoxKeyPair} from "tweetnacl";
 import util from "tweetnacl-util";
 import {encode} from "@stablelib/utf8";
+import {showError} from "./ToastFunctions";
+import {sha256} from "js-sha256";
 
 function getStringfromArray(array : Uint8Array) {
     return Buffer.from(util.encodeBase64(array), "base64").toString("hex");
@@ -42,6 +44,44 @@ export function verifyTransaction(t: transcation, signatureArray : Uint8Array, a
     let messageArr = getArrayfromString(message);
 
     return nacl.sign.detached.verify(messageArr, signatureArray, addressArray);
+}
+
+export function generateBlockHash(b : block, transactions: transcation[], accounts: account[]) : string {
+    let transactionArray = [];
+    for(let i = 0; i < b.transactions.length; i++) {
+        let t = transactions[b.transactions[i]];
+        if(t.from !== undefined) {
+            let account = accounts[t.from];
+
+            if(t.signatureArray !== undefined && account.addressArray !== undefined) {
+                let verified = verifyTransaction(t, t.signatureArray, account.addressArray);
+                if(!verified) {
+                    showError("Error: Signature in transaction " + t.id + " could not be verified!");
+                    console.log("Error: Signature in transaction " + t.id + " could not be verified!");
+                    return "";
+                }
+
+                transactionArray.push(transactionToString(t));
+            } else {
+                showError("Error while verifying transactions");
+                console.log("Error while verifying transactions: Signature-array or address-array is undefined");
+                return "";
+            }
+        } else {
+            showError("Error while verifying transactions");
+            console.log("Error while verifying transactions: Transaction " + t.id + " has undefined from-attribute");
+            return "";
+        }
+    }
+
+    let obj = {
+        prevHash: b.prevHash,
+        transactions: transactionArray,
+        nonce: b.nonce
+    }
+
+    let blockString = JSON.stringify(obj);
+    return sha256(blockString);
 }
 
 function transactionToString(t :transcation) : string {
