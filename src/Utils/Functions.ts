6309b1d706497ccc30d98638db0c06fafb93c451
nilslambertz <nilslambertz@web.de>
2021-06-22 02:24:46 +0200

    added verifyAllBlockTransactions, removed transaction-validation from block-hashing-function

--- src/Utils/Functions.ts
+++ src/Utils/Functions.ts
@@ -2,7 +2,6 @@ import {account, block, keyAddressPair, signaturePair, transcation} from "./Inte
 import nacl, {BoxKeyPair} from "tweetnacl";
 import util from "tweetnacl-util";
 import {encode} from "@stablelib/utf8";
-import {showError} from "./ToastFunctions";
 import {sha256} from "js-sha256";
 
 function getStringfromArray(array : Uint8Array) {
@@ -46,32 +45,29 @@ export function verifyTransaction(t: transcation, signatureArray : Uint8Array, a
     return nacl.sign.detached.verify(messageArr, signatureArray, addressArray);
 }
 
-export function generateBlockHash(b : block, transactions: transcation[], accounts: account[]) : string {
+export function verifyAllBlockTransactions(b : block, transactions: transcation[], accounts: account[]) : boolean {
+    for(let i = 0; i < b.transactions.length; i++) {
+        let t = transactions[b.transactions[i]];
+        if (t.from !== undefined) {
+            let account = accounts[t.from];
+
+            if (t.signatureArray !== undefined && account.addressArray !== undefined) {
+                let verified = verifyTransaction(t, t.signatureArray, account.addressArray);
+                if (!verified) {
+                    console.log("Error in block " + t.id + ": Signature in transaction " + t.id + " could not be verified!");
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+export function generateBlockHash(b : block, transactions: transcation[]) : string {
     let transactionArray = [];
     for(let i = 0; i < b.transactions.length; i++) {
         let t = transactions[b.transactions[i]];
-        if(t.from !== undefined) {
-            let account = accounts[t.from];
-
-            if(t.signatureArray !== undefined && account.addressArray !== undefined) {
-                let verified = verifyTransaction(t, t.signatureArray, account.addressArray);
-                if(!verified) {
-                    showError("Error: Signature in transaction " + t.id + " could not be verified!");
-                    console.log("Error: Signature in transaction " + t.id + " could not be verified!");
-                    return "";
-                }
-
-                transactionArray.push(transactionToString(t));
-            } else {
-                showError("Error while verifying transactions");
-                console.log("Error while verifying transactions: Signature-array or address-array is undefined");
-                return "";
-            }
-        } else {
-            showError("Error while verifying transactions");
-            console.log("Error while verifying transactions: Transaction " + t.id + " has undefined from-attribute");
-            return "";
-        }
+        transactionArray.push(transactionToString(t));
     }
 
     let obj = {
