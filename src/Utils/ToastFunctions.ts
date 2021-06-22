8a3d4f799cb33e020ac47d4d97812e8f6d8d745c
nilslambertz <nilslambertz@web.de>
2021-06-22 02:58:21 +0200

    added variable for options, added function to show warning

--- src/Utils/ToastFunctions.ts
+++ src/Utils/ToastFunctions.ts
@@ -1,15 +1,24 @@
-import {toast} from "react-toastify";
+import {toast, ToastOptions} from "react-toastify";
+
+const warnStyle = {
+    backgroundColor: "#FF5226"
+}
+
+const options : ToastOptions = {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: false,
+    progress: undefined,
+    style: warnStyle
+}
 
 export function showError(msg: string) : void {
-    toast.error(msg, {
-        position: "top-right",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: false,
-        progress: undefined,
-    });
+    toast.error(msg, options);
+}
 
-    return;
+export function showWarning(msg: string) : void {
+    toast.warn(msg, {...options, style: warnStyle});
 }
