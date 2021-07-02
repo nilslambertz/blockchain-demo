f772635b17b22e9054bd3947e24a9efda7053afb
nilslambertz <nilslambertz@web.de>
2021-07-02 15:41:54 +0200

    time is now displayed in bold text

--- src/Components/LogList/LogList.tsx
+++ src/Components/LogList/LogList.tsx
@@ -13,7 +13,7 @@ class LogList extends React.Component<LogListProps, {}> {
         return <div id={"logs"} className={this.props.logsVisible ? "visible" : ""}>
             <div id={"logList"}>
                 {this.props.logs.map((v, i, a) => {
-                    return <div key={i} className={v.type}><span style={{ color: "white" }}>{v.time}</span> {v.message}</div>
+                    return <div key={i} className={v.type}><span style={{ color: "white", fontWeight: "bold" }}>{v.time}</span> {v.message}</div>
                 })}
             </div>
         </div>
