ce2413dfac01899f3c7dd2fd23eaa385444b770c
nilslambertz <nilslambertz@web.de>
2023-01-07 03:36:39 +0100

    add remaining constants

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,4 +1,33 @@
+import { Block } from "../Utils/Interfaces";
+
 export const BORDER_COLOR = "border-gray-500";
 
 export const TRANSACTION_LIST_DROPPABLE_ID = "TRANSACTION_LIST";
 export const BLOCK_DROPPABLE_PREFIX = "BLOCK_";
+export const TRANSACTION_DRAGGABLE_PREFIX = "TRANSACTION_";
+
+export const VALID_START_HASH = "0".repeat(3);
+export const MAX_INITIAL_BALANCE = 1_000;
+export const MAX_MINING_HASH_ITERATIONS = 1_000_000;
+
+export const INITIAL_BLOCKS: Block[] = [
+  {
+    id: 0,
+    prevHash: VALID_START_HASH,
+    nonce: 0,
+    transactions: [],
+    confirmed: false,
+  },
+  {
+    id: 1,
+    nonce: 0,
+    transactions: [],
+    confirmed: false,
+  },
+];
+
+export const TIME_FORMATTER_OPTIONS: Intl.DateTimeFormatOptions = {
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+};
